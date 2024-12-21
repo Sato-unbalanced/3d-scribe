71ee20880e0fa8dd955526d9757d52c1c792ac69
Daniel Garcia <dangarc321@csu.fullerton.edu>
2024-12-21 15:02:29 -0800

    staring with project svelte template  application with some modifications already in place

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  
+  <nav>
+    <h1>3D-Scribe</h1>
+    <button>load object</button>
+    <button>upload object</button>
+  </nav>
+  
+  <div class="card">
+    <Counter />
+  </div>
+
+ 
+</main>
+
