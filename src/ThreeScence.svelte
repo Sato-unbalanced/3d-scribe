1f011bd8a4a63a386d8a10befd1d470f4535cea6
Daniel Garcia <dangarc321@csu.fullerton.edu>
2024-12-21 19:01:58 -0800

    added orbital control first version

--- src/ThreeScence.svelte
+++ src/ThreeScence.svelte
@@ -2,26 +2,44 @@
     import { onMount } from "svelte";
     import * as THREE from "three";
     import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader.js'
+    import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
   
     let container;
   
     onMount(() => {
       // Scene
       const scene = new THREE.Scene();
+     // scene.background = new THREE.Color(0xFFFFFF);
   
       // Camera
-      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
       camera.position.z = 5;
   
       // Renderer
       const renderer = new THREE.WebGLRenderer();
       renderer.setSize(window.innerWidth, window.innerHeight);
       container.appendChild(renderer.domElement);
+
+      //orbital controls
+      const controls = new OrbitControls(camera, renderer.domElement);
+      camera.position.set(0,20,100);
+      controls.update();
+
+
+      // AmbientLight 
+      const color = 0xFFFFFF;
+      const intensity = 1;
+      const ambient_light = new THREE.AmbientLight(color, intensity);
+      scene.add(ambient_light);
   
+      /*
       // Light 
       const light = new THREE.DirectionalLight(0xffffff, 1); 
       light.position.set(0, 1, 1).normalize(); 
       scene.add(light); 
+
+  */
+
       // Load OBJ 
       const objLoader = new OBJLoader(); 
       objLoader.load('../../airboat.obj', (object) => { 
@@ -30,6 +48,7 @@
         // Animation loop 
       const animate = function () { 
         requestAnimationFrame(animate); 
+        controls.update();
         renderer.render(scene, camera); 
       }; 
       
