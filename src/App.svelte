0b4da5656cfcb246c87aa0791175eb47fa5de4d2
Daniel Garcia <dangarc321@csu.fullerton.edu>
2024-12-27 15:23:20 -0800

    feature added to load obj filed from local storage as well as some formating changes for a better view of the content

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,180 @@
 <script>
+    import { onMount } from 'svelte';
   import ThreeScence from './ThreeScence.svelte'
+
+  let icon = "\u003E"
+  onMount(() =>{
+  var collapsibles = document.getElementsByClassName("collapsible");
+  for(var i = 0; i < collapsibles.length; i++)
+  {
+    collapsibles[i].addEventListener('click', function() {
+      this.classList.toggle("active");
+      var content2 = this.nextElementSibling;
+      if(content2.style.maxWidth)
+      {
+        icon = "\u003E";
+        content2.style.maxWidth = null;
+        content2.style.maxHeight = null;
+      }
+      else
+      {
+        icon = "\u003C"
+        content2.style.maxWidth = content2.scrollWidth + "px";
+        content2.style.maxHeight = "100%";
+
+      }
+    });
+  }
+});
 </script>
 
 <main>
-  
-  <nav>
-    <p>3d-Scribe</p>
+    <nav>
+    <h1>3d-Scribe</h1>
     <button>backgound color</button>
     <button>save</button>
     <button>load object</button>
-
     <button>upload object</button>
-  </nav>
-    <ThreeScence/>
-  
+    <input type="file" id="fileInput" accept=".obj">
+    </nav>
+
+
+   <!--<div id="view-area">-->       
+      <div id="three-scence">
+        <ThreeScence/>
+      </div>
+
+      <!--  "rhis was failed version to add controls for loading to float over three scence"
+      <div id = "controls">
+        <button type="button" class="collapsible" id= files>k <span>{icon}</span></button>
+        <div id="content">
+          <p>this is ment to simulate content being here</p>
+        </div>
+
+        <button type="button" class="collapsible" id="change">k <span>{icon}</span></button>
+        <div id="change-content">
+          <p>this is ment to simulate content being here</p>
+        </div>
+      </div>
+      
+    </div>
+  --->
 </main>
 
+<style>
+  main{
+    display: grid;
+    grid-template-rows: 10% 90%;
+    position: relative;
+  }
+  #view-area{
+    margin: 0;
+    padding: 0;
+    grid-row: 2;
+    display: grid;
+    height: 100%;
+    position: relative;
+  }
+
+  #three-scence{
+    grid-row: 2;
+    padding: 0;
+    margin: 0;  
+    position: relative;
+  }
+ 
+  h1{
+    float: left;
+    text-align: center;
+    margin: 0px 20px 20px 10px;
+  }
+  button{
+    float:left;
+    margin: 10px 20px 0px 20px; 
+  }
+  input{
+    float: left;
+    margin: 10px 20px 0px 20px;
+  }
+  nav{
+    grid-row: 1;
+    padding: 1rem;
+    margin: 0;
+    height: 10%;
+    z-index: 0;
+    position: relative;
+  }
+/*
+  
+  .collapsible {
+    
+    position: relative;
+    z-index: 2;
+    background-color: #4b4b4b4b;
+    color: white;
+    cursor: pointer;
+    width: 20px;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    border: none;
+    text-align: left;
+    outline: none;
+    font-size: 20px;
+  }
+
+.active, .collapsible:hover {
+  background-color: #555;
+}
+
+.collapsible:after {
+  
+  color: white;
+  font-weight: bold;
+  float: right;
+  margin-left: 5px;
+}
+
+#controls{
+  display: table;
+  z-index: 1;
+  clear: both;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#content {
+  padding: 0 18px;
+  z-index: 2;
+  position: relative;
+  max-height: 0;
+  max-width: 0;
+  float: left;
+  overflow: hidden;
+  transition: max-width 0.2s ease-out;
+  background-color: #4b4b4b4b;
+}
+
+#change-content{
+  padding: 0 18px;
+  position: relative;
+  z-index: 3;
+  float: right;
+  max-height: 0;
+  max-width: 0;
+  overflow: hidden;
+  transition: max-width 0.2s ease-out;
+  background-color: #4b4b4b4b;
+}
+#change{
+  position: relative;
+  float: right;
+  z-index: 3;
+  height: 100%;
+}
+
+*/
+</style>
--- src/ThreeScence.svelte
+++ src/ThreeScence.svelte
@@ -1,50 +1,83 @@
 <script>
-    import { onMount } from "svelte";
-    import * as THREE from "three";
-    import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader.js'
-    import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
+  import { onMount } from "svelte";
+  import * as THREE from "three";
+  import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader.js'
+  import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
   
-    let container;
+  let container;
+  let objLoader1;
+  onMount(() => {
+    // Scene
+    const scene = new THREE.Scene();
+    // scene.background = new THREE.Color(0xFFFFFF);
+
+    // Camera
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.z = 5;
   
-    onMount(() => {
-      // Scene
-      const scene = new THREE.Scene();
-     // scene.background = new THREE.Color(0xFFFFFF);
-  
-      // Camera
-      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-      camera.position.z = 5;
-  
-      // Renderer
-      const renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      container.appendChild(renderer.domElement);
+    // Renderer
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight*0.9);
+    container.appendChild(renderer.domElement);
 
-      //orbital controls
-      const controls = new OrbitControls(camera, renderer.domElement);
-      camera.position.set(0,20,100);
-      controls.update();
+    //orbital controls
+    const controls = new OrbitControls(camera, renderer.domElement);
+    camera.position.set(0,20,100);
+    controls.update();
 
-
-      // AmbientLight 
-      const color = 0xFFFFFF;
-      const intensity = 1;
-      const ambient_light = new THREE.AmbientLight(color, intensity);
-      scene.add(ambient_light);
-  
-      /*
-      // Light 
-      const light = new THREE.DirectionalLight(0xffffff, 1); 
-      light.position.set(0, 1, 1).normalize(); 
-      scene.add(light); 
-
-  */
-
+    // AmbientLight 
+    const color = 0xFFFFFF;
+    const intensity = 0.25;
+    const ambient_light = new THREE.AmbientLight(color, intensity);
+    scene.add(ambient_light);  
+      
+    // top Light 
+    const top_light = new THREE.DirectionalLight(0xffffff, 1); 
+    top_light.position.set(0, 1, 1).normalize(); 
+    scene.add(top_light); 
+/*
       // Load OBJ 
       const objLoader = new OBJLoader(); 
       objLoader.load('../../airboat.obj', (object) => { 
         scene.add(object); 
-      }); 
+
+        const boundingBox = new THREE.Box3().setFromObject(object);
+        const center = boundingBox.getCenter(new THREE.Vector3());
+        const size = boundingBox.getSize(new THREE.Vector3());
+
+        const cameraDistance = Math.max(size.x, size.y, size.z) * 0.75;
+
+        camera.position.set(center.x, center.y, center.z + cameraDistance);
+        camera.lookAt(center);
+
+
+        const model = object.scene;
+        const geometry = new THREE.BoxGeometry(1,1,1);
+        const material = new THREE.MeshBasicMaterial({color: 0x000000});
+        const mesh = new THREE.Mesh(geometry,material);
+        mesh.position.set(2,0,0);
+
+        model.add(mesh);
+
+      }); */
+
+    objLoader1 = new OBJLoader();
+
+    document.getElementById('fileInput').addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        if(file){
+          const reader = new FileReader();
+          reader.onload = function (e){
+            const contents = e.target.result;
+            const objFile = new Blob([contents], {type:"application/octet-stream"});
+            const url = URL.createObjectURL(objFile);
+            localStorage.setItem('objModelUrl', url);
+            loadModel(url);
+            };
+          reader.readAsArrayBuffer(file);
+        }
+      });
+
         // Animation loop 
       const animate = function () { 
         requestAnimationFrame(animate); 
@@ -57,17 +90,47 @@
       window.addEventListener('resize', () => { 
         camera.aspect = window.innerWidth / window.innerHeight; 
         camera.updateProjectionMatrix(); 
-        renderer.setSize(window.innerWidth, window.innerHeight); 
+        renderer.setSize(window.innerWidth, window.innerHeight*0.9); 
       });
-    });
-  
-  </script>
-  
-  <style>
-    canvas {
-      display: block;
+
+
+      function loadModel(url){
+      objLoader1.load(url, object =>{
+        scene.add(object); 
+        const boundingBox = new THREE.Box3().setFromObject(object);
+        const center = boundingBox.getCenter(new THREE.Vector3());
+        const size = boundingBox.getSize(new THREE.Vector3());
+
+        const cameraDistance = Math.max(size.x, size.y, size.z) * 0.75;
+
+        camera.position.set(center.x, center.y, center.z + cameraDistance);
+        camera.lookAt(center);
+
+        URL.revokeObjectURL(url);
+      });
     }
-  </style>
+
+
+    window.addEventListener('load', ()=>{
+      console.log("page was reloaded");
+      const savedUrl = localStorage.getItem('objModelUrl');
+      console.log(localStorage.getItem('objModelUrl'));
+      if(savedUrl){
+        console.log("trying to load in file that was saved locally")
+        loadModel(savedUrl);
+      }
+    });
+
+
+    });  
+</script>
   
-  <div bind:this={container}></div>
+<style>
+  canvas {
+    display: block;
+  }
+
+</style>
+  
+<div bind:this={container}></div>
   
